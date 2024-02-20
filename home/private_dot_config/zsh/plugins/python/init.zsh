0435620cc9fd633670aaa6523e4ce947a62d6cd1
Tho Nguyen <contact@naiwaaa.simplelogin.com>
2024-02-20 19:15:00 +0700

    revert: "feat(zsh): add an env variable to python plugin"
    Refs: 5948d51

--- home/private_dot_config/zsh/plugins/python/init.zsh
+++ home/private_dot_config/zsh/plugins/python/init.zsh
@@ -6,8 +6,6 @@ export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc"
 export IPYTHONDIR="$XDG_CONFIG_HOME"/ipython
 export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME"/jupyter
 
-export PIP_REQUIRE_VIRTUALENV=true
-
 # -------
 # Aliases
 # -------
