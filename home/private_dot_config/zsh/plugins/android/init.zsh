3cf12ee3ec60ab2f73383b0b85d8705b4603fec2
Tho Nguyen <contact@naiwaaa.simplelogin.com>
2024-04-03 13:16:55 +0700

    feat(zsh): add android plugin

---
+++ home/private_dot_config/zsh/plugins/android/init.zsh
@@ -0,0 +1,12 @@
+# ---------
+# Variables
+# ---------
+
+export ANDROID_USER_HOME="$XDG_DATA_HOME"/android
+export ANDROID_HOME="$XDG_DATA_HOME"/android/sdk
+
+# -------
+# Aliases
+# -------
+
+alias adb="HOME="$XDG_DATA_HOME"/android adb"
