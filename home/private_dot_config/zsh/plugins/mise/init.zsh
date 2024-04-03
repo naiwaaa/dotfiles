d6204d262040054bcfee3b568fc8cb783cb52981
Tho Nguyen <contact@naiwaaa.simplelogin.com>
2024-04-03 13:15:14 +0700

    fix(mise): use python@system by default

--- home/private_dot_config/zsh/plugins/mise/init.zsh
+++ home/private_dot_config/zsh/plugins/mise/init.zsh
@@ -9,6 +9,13 @@ fi
 export MISE_NODE_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME"/mise/default-node-packages
 export MISE_PYTHON_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME"/mise/default-python-packages
 
+# -------
+# Aliases
+# -------
+
+alias mx="mise exec"
+alias mr="mise run"
+
 # --------------
 # Initialization
 # --------------
