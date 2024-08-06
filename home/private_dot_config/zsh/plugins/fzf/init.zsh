dddfcae7eb88bb1da9fa263819e9c54ae12ba8c6
Tho Nguyen <contact@naiwaaa.simplelogin.com>
2024-08-06 17:54:52 +0700

    feat: use fzf default theme

--- home/private_dot_config/zsh/plugins/fzf/init.zsh
+++ home/private_dot_config/zsh/plugins/fzf/init.zsh
@@ -2,19 +2,8 @@ if ((!$+commands[fzf])); then
   return 1
 fi
 
-local rose_pine="
-	--color=fg:#908caa,bg:#191724,hl:#ebbcba
-	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
-	--color=border:#403d52,header:#31748f,gutter:#191724
-	--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
-	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
-
-export FZF_COLOR_SCHEME=${rose_pine}
-unset rose_pine
-
 export FZF_DEFAULT_COMMAND="fd --hidden --follow --exclude '.git'"
 export FZF_DEFAULT_OPTS="
-  ${FZF_COLOR_SCHEME}
   --multi
   --cycle
   --height=80%
