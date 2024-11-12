920544e1ec4628e59bb847ab5e7108a53521944a
Tho Nguyen <contact@naiwaaa.simplelogin.com>
2024-11-12 21:45:07 +0700

    feat(python): enable python dev mode

--- home/private_dot_config/zsh/plugins/python/init.zsh
+++ home/private_dot_config/zsh/plugins/python/init.zsh
@@ -6,6 +6,12 @@ export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc"
 export IPYTHONDIR="$XDG_CONFIG_HOME"/ipython
 export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME"/jupyter
 
+# Python development mode
+export PYTHONDEVMODE=1
+
+# do not write .pyc files
+export PYTHONDONTWRITEBYTECODE=1
+
 # -------
 # Aliases
 # -------
